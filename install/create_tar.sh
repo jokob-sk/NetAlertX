0a3a067b1285fe68e7bb49bf44119adbf5e4f655
pucherot <pucherot@users.noreply.github.com>
2021-01-09 14:05:51 +0100

    pialert.tar update

--- install/create_tar.sh
+++ install/create_tar.sh
@@ -1,4 +1,3 @@
-cd ../..
-rm pialert_2.50.tar
-tar cvf pialert/install/pialert_2.50.tar --exclude="pialert/install" --exclude="pialert/.git" pialert
+rm pialert/install/pialert_2.50.tar
+tar cvf pialert/install/pialert_2.50.tar -h --exclude="pialert/install" --exclude="pialert/.git" pialert
 
