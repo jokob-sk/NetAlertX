58f9ca8516dbcb4837909481c1a2f37f7276fd21
pucherot <pucherot@users.noreply.github.com>
2021-01-09 20:41:56 +0100

    Tar

--- install/create_tar.sh
+++ install/create_tar.sh
@@ -1,7 +1,8 @@
+#
 PIALERT_VERSION=2.50
 PIALERT_DEV_PATH=/media/WD_4TB/dev
 
 cd $PIALERT_DEV_PATH
 rm pialert/install/pialert_$PIALERT_VERSION.tar
-tar cvf pialert/install/pialert_$PIALERT_VERSION.tar --exclude="pialert/install" --exclude="pialert/.git" pialert
+tar cvf pialert/install/pialert_$PIALERT_VERSION.tar --exclude="pialert/install" --exclude="pialert/.git" pialert | wc -l
 
