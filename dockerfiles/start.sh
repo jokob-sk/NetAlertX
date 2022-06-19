953c306d73eaea9e25256f31a1ce4180d6609dd5
Macleykun <wesley.de.vree@gmail.com>
2022-06-19 21:47:47 +0200

    ...

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -2,4 +2,4 @@
 
 chmod -R o+w /home/pi/pialert/db
 # /etc/init.d/lighttpd start
-service cron start && /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
+service cron start && /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf &
