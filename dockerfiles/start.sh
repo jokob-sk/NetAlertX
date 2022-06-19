f9016d442b51f122af4daeb6401328152a315fba
Macleykun <wesley.de.vree@gmail.com>
2022-06-19 21:55:39 +0200

    return and just leave it like so and look at this in a seperate pr

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 chmod -R o+w /home/pi/pialert/db
-# /etc/init.d/lighttpd start
-service cron start && nohup /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf &
+/etc/init.d/lighttpd start
+cron && tail -f /var/log/syslog
