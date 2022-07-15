a3f4a9ddd1cc6ca0ed59cd1c181f0eb93dbe095b
jokob.sk <jokob.sk@gmail.com>
2022-07-15 12:33:12 +1000

    fixing permissions, simplyfying the start.sh script

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 /home/pi/pialert/dockerfiles/user-mapping.sh
-# probably too broad permissions
-chmod -R 755 /home/pi/pialert  
-chmod -R 755 /var/www/html/pialert
-chmod -R o+w /home/pi/pialert/db
+
+
+#chmod -R 755 /var/www/html/pialert
+#chmod -R o+w /home/pi/pialert/db
 /etc/init.d/lighttpd start
-service cron start && tail -f /dev/null
+cron -f
