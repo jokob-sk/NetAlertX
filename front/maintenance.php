0b3ba217764edcc40a709fa7fa2d214a3113e7d3
leiweibau <105860611+leiweibau@users.noreply.github.com>
2022-07-22 23:35:45 +0200

    Translation work continued
    - maintenance page (tab titel)

--- front/maintenance.php
+++ front/maintenance.php
@@ -217,9 +217,9 @@ if (submit && isset($_POST['langselector_set'])) {
 
     <div class="nav-tabs-custom">
     <ul class="nav nav-tabs">
-        <li class="active"><a href="#tab_Settings" data-toggle="tab">Settings</a></li>
-        <li><a href="#tab_DBTools" data-toggle="tab">DB Tools</a></li>
-        <li><a href="#tab_BackupRestore" data-toggle="tab">Backup / Restore</a></li>
+        <li class="active"><a href="#tab_Settings" data-toggle="tab"><?php echo $pia_lang['Maintenance_Tools_Tab_Settings'];?></a></li>
+        <li><a href="#tab_DBTools" data-toggle="tab"><?php echo $pia_lang['Maintenance_Tools_Tab_Tools'];?></a></li>
+        <li><a href="#tab_BackupRestore" data-toggle="tab"><?php echo $pia_lang['Maintenance_Tools_Tab_BackupRestore'];?></a></li>
     </ul>
     <div class="tab-content">
         <div class="tab-pane active" id="tab_Settings">
