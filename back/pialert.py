28d163cf0b5a518329a2b7d476f29a220ac125de
leiweibau <105860611+leiweibau@users.noreply.github.com>
2022-07-29 16:41:38 +0200

    Merge Comit https://github.com/jokob-sk/Pi.Alert/commit/6c4e354f5ffdc1be83d21c10c078297245e69131

--- back/pialert.py
+++ back/pialert.py
@@ -1215,8 +1215,8 @@ def email_reporting ():
     mail_text = mail_text.replace ('<REPORT_DATE>', timeFormated)
     mail_html = mail_html.replace ('<REPORT_DATE>', timeFormated)
 
-    mail_text = mail_text.replace ('<SCAN_CYCLE>', cycle )
-    mail_html = mail_html.replace ('<SCAN_CYCLE>', cycle )
+    # mail_text = mail_text.replace ('<SCAN_CYCLE>', cycle )
+    # mail_html = mail_html.replace ('<SCAN_CYCLE>', cycle )
 
     mail_text = mail_text.replace ('<SERVER_NAME>', socket.gethostname() )
     mail_html = mail_html.replace ('<SERVER_NAME>', socket.gethostname() )
