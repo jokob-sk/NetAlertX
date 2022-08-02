734c488ca0279ecdc173a47a9d2c372be0f8403b
leiweibau <105860611+leiweibau@users.noreply.github.com>
2022-08-02 21:05:12 +0200

    fix DB Error and update archive

--- install/pialert_install.sh
+++ install/pialert_install.sh
@@ -454,7 +454,7 @@ download_pialert() {
   fi
   
   print_msg "- Downloading installation tar file..."
-  curl -Lo "$INSTALL_DIR/pialert_latest.tar" https://raw.githubusercontent.com/leiweibau/Pi.Alert/setup_test/tar/pialert_latest.tar
+  curl -Lo "$INSTALL_DIR/pialert_latest.tar" https://github.com/leiweibau/Pi.Alert/raw/setup_test/tar/pialert_latest.tar
   echo ""
 
   print_msg "- Uncompressing tar file"
