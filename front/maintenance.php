2e713bf1d0d2da1c2eb5e4d5daf7e00cf8a2d960
Jokob-sk <jokob.sk@gmail.com>
2023-06-18 07:40:02 +1000

    Fix Log Limit #255

--- front/maintenance.php
+++ front/maintenance.php
@@ -338,7 +338,17 @@ if (isset($_POST['submit']) && submit && isset($_POST['skinselector_set'])) {
                     <div class="db_info_table">
                         <div class="log-area">
                             <div class="row logs-row">
-                              <textarea id="pialert_log" class="logs" cols="70" rows="10" wrap='off' readonly ><?php echo file_get_contents( "./log/pialert.log", false, null, -200000); ?>
+                              <textarea id="pialert_log" class="logs" cols="70" rows="10" wrap='off' readonly >
+                                <?php                               
+                                if(filesize("./log/pialert.log") > 200000)
+                                {
+                                  echo file_get_contents( "./log/pialert.log", false, null, -200000); 
+                                }
+                                else{
+                                  echo file_get_contents( "./log/pialert.log" );
+                                }
+                              
+                              ?>
                               </textarea>
                             </div>
                             <div class="row logs-row" >
