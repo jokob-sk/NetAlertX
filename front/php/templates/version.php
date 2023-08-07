9bdac0edbf41e4775fc724ac0483ef7bc26be675
Carlos V <76731844+cvc90@users.noreply.github.com>
2023-08-07 17:44:40 +0200

    Update version.php
    Created creators banner

--- front/php/templates/version.php
+++ front/php/templates/version.php
@@ -1,4 +1,16 @@
-<?php      
+<?php   
+#---------------------------------------------------------------------------------#
+#  Pi.Alert                                                                       #
+#  Open Source Network Guard / WIFI & LAN intrusion detector                      #  
+#                                                                                 #
+#  version.php - Templates module Template to display the current version         #
+#---------------------------------------------------------------------------------#
+#    Puche      2021        pi.alert.application@gmail.com   GNU GPLv3            #
+#    jokob-sk   2022        jokob.sk@gmail.com               GNU GPLv3            #
+#    leiweibau  2022        https://github.com/leiweibau     GNU GPLv3            #
+#    cvc90      2023        https://github.com/cvc90         GNU GPLv3            #
+#---------------------------------------------------------------------------------#
+   
 $filename = "/.VERSION";
 if(file_exists($filename))
 {
