b068b631be35e3d4e3e8bc341193953370ffbad3
Carlos V <76731844+cvc90@users.noreply.github.com>
2023-08-07 18:14:21 +0200

    Update build.php
    Added the creators banner
    Updated blank spaces

--- front/php/templates/build.php
+++ front/php/templates/build.php
@@ -1,8 +1,21 @@
 <?php
-	$file = "/home/pi/pialert/front/buildtimestamp.txt";
-	if (file_exists($file)) {
-		echo date("Y-m-d", ((int)file_get_contents($file)));
-	} else {
-		echo "File not found";
-	}
+#---------------------------------------------------------------------------------#
+#  Pi.Alert                                                                       #
+#  Open Source Network Guard / WIFI & LAN intrusion detector                      #  
+#                                                                                 #
+#  version.php - Templates module Template to display the current version         #
+#---------------------------------------------------------------------------------#
+#    Puche      2021        pi.alert.application@gmail.com   GNU GPLv3            #
+#    jokob-sk   2022        jokob.sk@gmail.com               GNU GPLv3            #
+#    leiweibau  2022        https://github.com/leiweibau     GNU GPLv3            #
+#    cvc90      2023        https://github.com/cvc90         GNU GPLv3            #
+#---------------------------------------------------------------------------------#
+
+    $file = "/home/pi/pialert/front/buildtimestamp.txt";
+    if (file_exists($file)) {
+        echo date("Y-m-d", ((int)file_get_contents($file)));
+    }
+    else {
+        echo "File not found";
+    }
 ?>
