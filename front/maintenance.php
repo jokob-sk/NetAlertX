b1b3abeb2f0723f335415724c048ca16c15f6d11
Carlos V <76731844+cvc90@users.noreply.github.com>
2023-08-11 01:28:34 +0200

    Update maintenance.php
    Updated title "Maintenance_Title"

--- front/maintenance.php
+++ front/maintenance.php
@@ -44,7 +44,7 @@ $pia_installed_skins = array('skin-black-light',
     <section class="content-header">
     <?php require 'php/templates/notification.php'; ?>
       <h1 id="pageTitle">
-        <i class=\"fa fa-wrench\"></i></i>         
+        <i class=\"fa fa-wrench\"></i>         
         <?= lang('Maintenance_Title');?>
       </h1>
     </section>
