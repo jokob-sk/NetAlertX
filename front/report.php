243f0034f3d061731114cf11f88cea5410ecdda1
Carlos V <76731844+cvc90@users.noreply.github.com>
2023-08-10 21:50:21 +0200

    Update report.php
    Modified the creators banner
    Removed error_reporting variable to disable php errors

--- front/report.php
+++ front/report.php
@@ -1,18 +1,17 @@
 <?php
 
-//------------------------------------------------------------------------------
-//  Pi.Alert
-//  Open Source Network Guard / WIFI & LAN intrusion detector 
-//
-//  report.php - Front module. Server side. Manage Devices
-//------------------------------------------------------------------------------
-//  Puche      2021        pi.alert.application@gmail.com   GNU GPLv3
-//  jokob-sk   2022        jokob.sk@gmail.com               GNU GPLv3
-//  leiweibau  2022        https://github.com/leiweibau     GNU GPLv3
-//  cvc90      2023        https://github.com/cvc90         GNU GPLv3
-//------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------#
+#  Pi.Alert                                                                       #
+#  Open Source Network Guard / WIFI & LAN intrusion detector                      #  
+#                                                                                 #
+#  report.php - Front module. Server side. Manage Devices                         #
+#---------------------------------------------------------------------------------#
+#    Puche      2021        pi.alert.application@gmail.com   GNU GPLv3            #
+#    jokob-sk   2022        jokob.sk@gmail.com               GNU GPLv3            #
+#    leiweibau  2022        https://github.com/leiweibau     GNU GPLv3            #
+#    cvc90      2023        https://github.com/cvc90         GNU GPLv3            #
+#---------------------------------------------------------------------------------#
 
-  error_reporting(0);// Turn off php errors
   require 'php/templates/header.php';
   
 ?>
