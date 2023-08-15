80888ae910eb6fbc76f3b9ab49401bd7fc9c4b25
Carlos V <76731844+cvc90@users.noreply.github.com>
2023-08-15 18:32:55 +0200

    Update deviceDetails.php
    Updated creators banner

--- front/deviceDetails.php
+++ front/deviceDetails.php
@@ -1,11 +1,16 @@
-<!-- ---------------------------------------------------------------------------
-#  Pi.Alert
-#  Open Source Network Guard / WIFI & LAN intrusion detector 
-#
-#  deviceDetails.php - Front module. Device management page
-#-------------------------------------------------------------------------------
-#  Puche 2021 / 2022+ jokob             jokob@duck.com                GNU GPLv3
-#--------------------------------------------------------------------------- -->
+<!--
+#---------------------------------------------------------------------------------#
+#  Pi.Alert                                                                       #
+#  Open Source Network Guard / WIFI & LAN intrusion detector                      #  
+#                                                                                 #
+#  deviceDetails.php - Front module. Device management page                       #
+#---------------------------------------------------------------------------------#
+#    Puche      2021        pi.alert.application@gmail.com   GNU GPLv3            #
+#    jokob-sk   2022        jokob.sk@gmail.com               GNU GPLv3            #
+#    leiweibau  2022        https://github.com/leiweibau     GNU GPLv3            #
+#    cvc90      2023        https://github.com/cvc90         GNU GPLv3            #
+#---------------------------------------------------------------------------------#
+-->
 
 <?php
   require 'php/templates/header.php';
