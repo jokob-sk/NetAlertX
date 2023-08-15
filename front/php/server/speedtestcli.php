385deb254b56662e7c6871cce2038e82a791bdf0
Carlos V <76731844+cvc90@users.noreply.github.com>
2023-08-15 19:04:45 +0200

    Update speedtestcli.php
    Updated translation variable "Speedtest_Results"

--- front/php/server/speedtestcli.php
+++ front/php/server/speedtestcli.php
@@ -1,7 +1,7 @@
 <?php
 exec('../../../back/speedtest-cli --secure --simple', $output);
 
-echo '<h4>. lang('Speedtest_Results') .</h4>';
+echo '<h4>'. lang('Speedtest_Results') .'</h4>';
 echo '<pre style="border: none;">'; 
 foreach($output as $line){
     echo $line . "\n";
