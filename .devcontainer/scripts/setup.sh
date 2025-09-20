c3dc04c1e5b3390656f217eb3adfbd797aee6c3e
Adam Outler <adamoutler@gmail.com>
2025-09-20 21:26:09 +0000

    use proper db for setup

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -69,13 +69,13 @@ configure_source() {
     echo "Dev">${INSTALL_DIR}/.VERSION
     safe_link ${SOURCE_DIR}/api ${INSTALL_DIR}/api
     safe_link ${SOURCE_DIR}/back ${INSTALL_DIR}/back
-    if [ ! -f "${SOURCE_DIR}/config/app.conf" ]; then
-        cp ${SOURCE_DIR}/back/app.conf ${INSTALL_DIR}/config/
-        rm /workspaces/NetAlertX/db/app.db
-    fi
-
     safe_link "${SOURCE_DIR}/config"         "${INSTALL_DIR}/config"
     safe_link "${SOURCE_DIR}/db"         "${INSTALL_DIR}/db"
+    if [ ! -f "${SOURCE_DIR}/config/app.conf" ]; then
+        cp ${SOURCE_DIR}/back/app.conf ${INSTALL_DIR}/config/
+        cp ${SOURCE_DIR}/back/app.db ${INSTALL_DIR}/db/
+    fi
+
     safe_link "${SOURCE_DIR}/docs"       "${INSTALL_DIR}/docs"
     safe_link "${SOURCE_DIR}/front"      "${INSTALL_DIR}/front"
     safe_link "${SOURCE_DIR}/install"    "${INSTALL_DIR}/install"
