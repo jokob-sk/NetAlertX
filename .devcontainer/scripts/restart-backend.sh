47f23fcc4f328d1e69037a44332a2a84c7670401
Adam Outler <adamoutler@gmail.com>
2025-09-22 21:55:55 -0400

    Rework Logging in restart-backend.sh
    The stdout and stderr are useful logs when debugging and trying to figure out why plugin output is causing backend to stop and exception. This commit enables output redirection to `/app/stdout.log` and `/app/stderr.log` from the backend.  This may need backporting to production as it appears the fields are unused in the backend. 
    
    Additionally, when searching logs in the UI, the old logs appear first and your search results will invariably find old information when searching with ctrl-f-"string"-enter. So upon backend start and to keep them relevant, the stdout, stderr, and app logs are cleared.

--- .devcontainer/scripts/restart-backend.sh
+++ .devcontainer/scripts/restart-backend.sh
@@ -14,11 +14,11 @@ PORT_DEBUG=5678
 sudo killall python3 2>/dev/null || true
 sleep 2
 
+echo ''|tee -a $LOG_DIR/stdout.log $LOG_DIR/stderr.log $LOG_DIR/app.log
 
 cd "$APP_DIR"
 
 # Launch using absolute module path for clarity; rely on cwd for local imports
-setsid nohup ${PY} -m debugpy --listen 0.0.0.0:${PORT_DEBUG} /app/server/__main__.py >/dev/null 2>&1 &
+setsid nohup ${PY} -m debugpy --listen 0.0.0.0:${PORT_DEBUG} /app/server/__main__.py 1>>/app/log/stdout.log 2>>/app/log/stderr.log &
 PID=$!
 sleep 2
-
