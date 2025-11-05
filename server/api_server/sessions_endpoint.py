85aa04c490c04ef9ece6ce61d8b590fc23225091
jokob-sk <jokob.sk@gmail.com>
2025-11-06 08:14:00 +1100

    TEST: fix
    Signed-off-by: jokob-sk <jokob.sk@gmail.com>

--- server/api_server/sessions_endpoint.py
+++ server/api_server/sessions_endpoint.py
@@ -270,6 +270,7 @@ def get_session_events(event_type, period_date):
     conn = get_temp_db_connection()
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
+    tz_name = get_setting_value("TIMEZONE") or "UTC"
 
     # Base SQLs
     sql_events = f"""
